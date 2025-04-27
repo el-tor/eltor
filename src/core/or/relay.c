192b0a791b50f9ad9560b14d583993b4ba87df36
eltordev <eltordev@proton.me>
2025-04-26 20:06:53 -0500

    yolo 2

--- src/core/or/relay.c
+++ src/core/or/relay.c
@@ -660,7 +660,8 @@ relay_send_command_from_edge_,(streamid_t stream_id, circuit_t *orig_circ,
   /* XXXX NM Split this function into a separate versions per circuit type? */
 
   tor_assert(circ);
-  tor_assert(payload_len <= RELAY_PAYLOAD_SIZE);
+  // TODO: yolo
+  //tor_assert(payload_len <= RELAY_PAYLOAD_SIZE);
 
   memset(&cell, 0, sizeof(cell_t));
   cell.command = CELL_RELAY;
